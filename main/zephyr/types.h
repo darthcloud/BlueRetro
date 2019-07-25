c2c7c52e26a5c1989155429fbad2a9e7ede68134
Jacques Gagnon <darthcloud@gmail.com>
2019-07-25 18:03:29 -0400

    Zephyr files

---
+++ main/zephyr/types.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_ZEPHYR_TYPES_H_
+#define ZEPHYR_INCLUDE_ZEPHYR_TYPES_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef signed char         s8_t;
+typedef signed short        s16_t;
+typedef signed int          s32_t;
+typedef signed long long    s64_t;
+
+typedef unsigned char       u8_t;
+typedef unsigned short      u16_t;
+typedef unsigned int        u32_t;
+typedef unsigned long long  u64_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_ZEPHYR_TYPES_H_ */
