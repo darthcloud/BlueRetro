5763d52518c6e4256e1ff95c0d0ca90e838287b2
Jacques Gagnon <darthcloud@gmail.com>
2019-10-20 13:25:11 -0400

    Fixup tid

--- main/bt_sdp.c
+++ main/bt_sdp.c
@@ -4,7 +4,7 @@
 #include "bt_host.h"
 #include "bt_sdp.h"
 
-static void bt_sdp_cmd(uint16_t handle, uint16_t cid, uint8_t code, uint8_t tid, uint16_t len) {
+static void bt_sdp_cmd(uint16_t handle, uint16_t cid, uint8_t code, uint16_t tid, uint16_t len) {
     uint16_t packet_len = (BT_HCI_H4_HDR_SIZE + BT_HCI_ACL_HDR_SIZE
         + sizeof(struct bt_l2cap_hdr) + sizeof(struct bt_sdp_hdr) + len);
 
@@ -17,7 +17,7 @@ static void bt_sdp_cmd(uint16_t handle, uint16_t cid, uint8_t code, uint8_t tid,
     bt_hci_pkt_tmp.l2cap_hdr.cid = cid;
 
     bt_hci_pkt_tmp.sdp_hdr.op_code = code;
-    bt_hci_pkt_tmp.sdp_hdr.tid = tid;
+    bt_hci_pkt_tmp.sdp_hdr.tid = sys_cpu_to_be16(tid);
     bt_hci_pkt_tmp.sdp_hdr.param_len = sys_cpu_to_be16(len);
 
     bt_host_txq_add((uint8_t *)&bt_hci_pkt_tmp, packet_len);
@@ -32,7 +32,7 @@ void bt_sdp_cmd_svc_search_attr_req(void *bt_dev) {
     bt_sdp_cmd(device->acl_handle, device->sdp_chan.dcid, BT_SDP_SVC_SEARCH_ATTR_REQ, device->sdp_chan.ident, sizeof(attr_req));
 }
 
-static void bt_sdp_cmd_svc_search_attr_rsp(uint16_t handle, uint16_t cid, uint8_t tid) {
+static void bt_sdp_cmd_svc_search_attr_rsp(uint16_t handle, uint16_t cid, uint16_t tid) {
     struct bt_sdp_att_rsp *att_rsp = (struct bt_sdp_att_rsp *)bt_hci_pkt_tmp.sdp_data;
     uint8_t *sdp_data = bt_hci_pkt_tmp.sig_data + sizeof(struct bt_sdp_att_rsp);
 
@@ -45,7 +45,7 @@ void bt_sdp_hdlr(struct bt_dev *device, struct bt_hci_pkt *bt_hci_acl_pkt) {
     switch (bt_hci_acl_pkt->sdp_hdr.op_code) {
         case BT_SDP_SVC_SEARCH_ATTR_REQ:
         {
-            bt_sdp_cmd_svc_search_attr_rsp(device->acl_handle, device->sdp_chan.dcid, bt_hci_acl_pkt->sdp_hdr.tid);
+            bt_sdp_cmd_svc_search_attr_rsp(device->acl_handle, device->sdp_chan.dcid, sys_be16_to_cpu(bt_hci_acl_pkt->sdp_hdr.tid));
             break;
         }
     }
