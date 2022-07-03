78c257134644ff4bd005a112c0bdead9fbe5d94a
Jacques Gagnon <darthcloud@gmail.com>
2022-07-03 15:37:54 -0400

    [ADAPTER] Add common turbo mask generator functions

--- main/adapter/wired/wired.c
+++ main/adapter/wired/wired.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Jacques Gagnon
+ * Copyright (c) 2021-2022, Jacques Gagnon
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -156,3 +156,86 @@ void wired_fb_to_generic(int32_t dev_mode, struct raw_fb *raw_fb_data, struct ge
         fb_to_generic_func[wired_adapter.system_id](dev_mode, raw_fb_data, fb_data);
     }
 }
+
+void IRAM_ATTR wired_gen_turbo_mask_btns16_pos(struct wired_data *wired_data, uint16_t *buttons, const uint32_t btns_mask[32]) {
+    for (uint32_t i = 0; i < 32; i++) {
+        uint8_t mask = wired_data->cnt_mask[i] >> 1;
+
+        if (btns_mask[i] && mask) {
+            if (wired_data->cnt_mask[i] & 1) {
+                if (!(mask & wired_data->frame_cnt)) {
+                    *buttons &= ~btns_mask[i];
+                }
+            }
+            else {
+                if (!((mask & wired_data->frame_cnt) == mask)) {
+                    *buttons &= ~btns_mask[i];
+                }
+            }
+        }
+    }
+}
+
+void IRAM_ATTR wired_gen_turbo_mask_btns16_neg(struct wired_data *wired_data, uint16_t *buttons, const uint32_t btns_mask[32]) {
+    for (uint32_t i = 0; i < 32; i++) {
+        uint8_t mask = wired_data->cnt_mask[i] >> 1;
+
+        if (btns_mask[i] && mask) {
+            if (wired_data->cnt_mask[i] & 1) {
+                if (!(mask & wired_data->frame_cnt)) {
+                    *buttons |= btns_mask[i];
+                }
+            }
+            else {
+                if (!((mask & wired_data->frame_cnt) == mask)) {
+                    *buttons |= btns_mask[i];
+                }
+            }
+        }
+    }
+}
+
+void IRAM_ATTR wired_gen_turbo_mask_btns32(struct wired_data *wired_data, uint32_t *buttons, const uint32_t (*btns_mask)[32],
+                                            uint32_t bank_cnt) {
+    for (uint32_t i = 0; i < 32; i++) {
+        uint8_t mask = wired_data->cnt_mask[i] >> 1;
+
+        if (mask) {
+            for (uint32_t j = 0; j < bank_cnt; j++) {
+                if (btns_mask[j][i]) {
+                    if (wired_data->cnt_mask[i] & 1) {
+                        if (!(mask & wired_data->frame_cnt)) {
+                            buttons[j] |= btns_mask[j][i];
+                        }
+                    }
+                    else {
+                        if (!((mask & wired_data->frame_cnt) == mask)) {
+                            buttons[j] |= btns_mask[j][i];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void IRAM_ATTR wired_gen_turbo_mask_axes8(struct wired_data *wired_data, uint8_t *axes, uint32_t axes_cnt,
+                                            const uint8_t axes_idx[6], const struct ctrl_meta *axes_meta) {
+    for (uint32_t i = 0; i < axes_cnt; i++) {
+        uint8_t btn_id = axis_to_btn_id(i);
+        uint8_t mask = wired_data->cnt_mask[btn_id] >> 1;
+        if (mask) {
+            if (wired_data->cnt_mask[btn_id] & 1) {
+                if (!(mask & wired_data->frame_cnt)) {
+                    axes[axes_idx[i]] = axes_meta[i].neutral;
+                }
+            }
+            else {
+                if (!((mask & wired_data->frame_cnt) == mask)) {
+                    axes[axes_idx[i]] = axes_meta[i].neutral;
+                }
+            }
+        }
+    }
+}
+
--- main/adapter/wired/wired.h
+++ main/adapter/wired/wired.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Jacques Gagnon
+ * Copyright (c) 2021-2022, Jacques Gagnon
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -11,5 +11,12 @@ int32_t wired_meta_init(struct generic_ctrl *ctrl_data);
 void wired_init_buffer(int32_t dev_mode, struct wired_data *wired_data);
 void wired_from_generic(int32_t dev_mode, struct generic_ctrl *ctrl_data, struct wired_data *wired_data);
 void wired_fb_to_generic(int32_t dev_mode, struct raw_fb *raw_fb_data, struct generic_fb *fb_data);
+void wired_gen_turbo_mask_btns16_pos(struct wired_data *wired_data, uint16_t *buttons, const uint32_t btns_mask[32]);
+void wired_gen_turbo_mask_btns16_neg(struct wired_data *wired_data, uint16_t *buttons, const uint32_t btns_mask[32]);
+void wired_gen_turbo_mask_btns32(struct wired_data *wired_data, uint32_t *buttons, const uint32_t (*btns_mask)[32],
+                                uint32_t bank_cnt);
+void wired_gen_turbo_mask_axes8(struct wired_data *wired_data, uint8_t *axes, uint32_t axes_cnt,
+                                const uint8_t axes_idx[6], const struct ctrl_meta *axes_meta);
 
 #endif /* _WIRED_H_ */
+
