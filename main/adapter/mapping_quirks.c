596af1f2753e5ed6503895d6d004bc8d77623239
Jacques Gagnon <darthcloud@gmail.com>
2022-07-22 13:48:39 -0400

    [QUIRKS] Rework functions parameters

--- main/adapter/mapping_quirks.c
+++ main/adapter/mapping_quirks.c
@@ -10,110 +10,110 @@
 #include "adapter.h"
 #include "mapping_quirks.h"
 
-static void face_btns_invert(uint32_t btns_mask[32]) {
-    uint32_t tmp = btns_mask[PAD_RB_LEFT];
+static void face_btns_invert(struct raw_src_mapping *map) {
+    uint32_t tmp = map->btns_mask[PAD_RB_LEFT];
 
-    btns_mask[PAD_RB_LEFT] = btns_mask[PAD_RB_UP];
-    btns_mask[PAD_RB_UP] = tmp;
+    map->btns_mask[PAD_RB_LEFT] = map->btns_mask[PAD_RB_UP];
+    map->btns_mask[PAD_RB_UP] = tmp;
 
-    tmp = btns_mask[PAD_RB_DOWN];
-    btns_mask[PAD_RB_DOWN] = btns_mask[PAD_RB_RIGHT];
-    btns_mask[PAD_RB_RIGHT] = tmp;
+    tmp = map->btns_mask[PAD_RB_DOWN];
+    map->btns_mask[PAD_RB_DOWN] = map->btns_mask[PAD_RB_RIGHT];
+    map->btns_mask[PAD_RB_RIGHT] = tmp;
 }
 
-static void face_btns_rotate_right(uint32_t btns_mask[32]) {
-    uint32_t tmp = btns_mask[PAD_RB_UP];
+static void face_btns_rotate_right(struct raw_src_mapping *map) {
+    uint32_t tmp = map->btns_mask[PAD_RB_UP];
 
-    btns_mask[PAD_RB_UP] = btns_mask[PAD_RB_LEFT];
-    btns_mask[PAD_RB_LEFT] = btns_mask[PAD_RB_DOWN];
-    btns_mask[PAD_RB_DOWN] = btns_mask[PAD_RB_RIGHT];
-    btns_mask[PAD_RB_RIGHT] = tmp;
+    map->btns_mask[PAD_RB_UP] = map->btns_mask[PAD_RB_LEFT];
+    map->btns_mask[PAD_RB_LEFT] = map->btns_mask[PAD_RB_DOWN];
+    map->btns_mask[PAD_RB_DOWN] = map->btns_mask[PAD_RB_RIGHT];
+    map->btns_mask[PAD_RB_RIGHT] = tmp;
 }
 
-static void trigger_pri_sec_invert(uint32_t btns_mask[32]) {
-    uint32_t tmp = btns_mask[PAD_LM];
+static void trigger_pri_sec_invert(struct raw_src_mapping *map) {
+    uint32_t tmp = map->btns_mask[PAD_LM];
 
-    btns_mask[PAD_LM] = btns_mask[PAD_LS];
-    btns_mask[PAD_LS] = tmp;
+    map->btns_mask[PAD_LM] = map->btns_mask[PAD_LS];
+    map->btns_mask[PAD_LS] = tmp;
 
-    tmp = btns_mask[PAD_RM];
-    btns_mask[PAD_RM] = btns_mask[PAD_RS];
-    btns_mask[PAD_RS] = tmp;
+    tmp = map->btns_mask[PAD_RM];
+    map->btns_mask[PAD_RM] = map->btns_mask[PAD_RS];
+    map->btns_mask[PAD_RS] = tmp;
 }
 
-static void sw_left_joycon(uint32_t btns_mask[32]) {
-    btns_mask[PAD_MM] = btns_mask[PAD_MQ];
-    btns_mask[PAD_MQ] = 0;
+static void sw_left_joycon(struct raw_src_mapping *map) {
+    map->btns_mask[PAD_MM] = map->btns_mask[PAD_MQ];
+    map->btns_mask[PAD_MQ] = 0;
 
-    btns_mask[PAD_RM] = btns_mask[PAD_RT];
-    btns_mask[PAD_RS] = btns_mask[PAD_LS];
-    btns_mask[PAD_RT] = 0;
+    map->btns_mask[PAD_RM] = map->btns_mask[PAD_RT];
+    map->btns_mask[PAD_RS] = map->btns_mask[PAD_LS];
+    map->btns_mask[PAD_RT] = 0;
 
-    btns_mask[PAD_LS] = btns_mask[PAD_LM];
-    btns_mask[PAD_LM] = btns_mask[PAD_LT];
-    btns_mask[PAD_LT] = 0;
+    map->btns_mask[PAD_LS] = map->btns_mask[PAD_LM];
+    map->btns_mask[PAD_LM] = map->btns_mask[PAD_LT];
+    map->btns_mask[PAD_LT] = 0;
 
-    btns_mask[PAD_RB_UP] = btns_mask[PAD_LD_RIGHT];
-    btns_mask[PAD_RB_LEFT] = btns_mask[PAD_LD_UP];
-    btns_mask[PAD_RB_DOWN] = btns_mask[PAD_LD_LEFT];
-    btns_mask[PAD_RB_RIGHT] = btns_mask[PAD_LD_DOWN];
+    map->btns_mask[PAD_RB_UP] = map->btns_mask[PAD_LD_RIGHT];
+    map->btns_mask[PAD_RB_LEFT] = map->btns_mask[PAD_LD_UP];
+    map->btns_mask[PAD_RB_DOWN] = map->btns_mask[PAD_LD_LEFT];
+    map->btns_mask[PAD_RB_RIGHT] = map->btns_mask[PAD_LD_DOWN];
 
-    btns_mask[PAD_LD_LEFT] = 0;
-    btns_mask[PAD_LD_DOWN] = 0;
-    btns_mask[PAD_LD_RIGHT] = 0;
-    btns_mask[PAD_LD_UP] = 0;
+    map->btns_mask[PAD_LD_LEFT] = 0;
+    map->btns_mask[PAD_LD_DOWN] = 0;
+    map->btns_mask[PAD_LD_RIGHT] = 0;
+    map->btns_mask[PAD_LD_UP] = 0;
 }
 
-static void sw_right_joycon(uint32_t btns_mask[32]) {
-    btns_mask[PAD_MS] = btns_mask[PAD_MT];
-    btns_mask[PAD_MT] = 0;
+static void sw_right_joycon(struct raw_src_mapping *map) {
+    map->btns_mask[PAD_MS] = map->btns_mask[PAD_MT];
+    map->btns_mask[PAD_MT] = 0;
 
-    btns_mask[PAD_LM] = btns_mask[PAD_LT];
-    btns_mask[PAD_LS] = btns_mask[PAD_RS];
-    btns_mask[PAD_LT] = 0;
-    btns_mask[PAD_LJ] = btns_mask[PAD_RJ];
+    map->btns_mask[PAD_LM] = map->btns_mask[PAD_LT];
+    map->btns_mask[PAD_LS] = map->btns_mask[PAD_RS];
+    map->btns_mask[PAD_LT] = 0;
+    map->btns_mask[PAD_LJ] = map->btns_mask[PAD_RJ];
 
-    btns_mask[PAD_RS] = btns_mask[PAD_RM];
-    btns_mask[PAD_RM] = btns_mask[PAD_RT];
-    btns_mask[PAD_RT] = 0;
-    btns_mask[PAD_RJ] = 0;
+    map->btns_mask[PAD_RS] = map->btns_mask[PAD_RM];
+    map->btns_mask[PAD_RM] = map->btns_mask[PAD_RT];
+    map->btns_mask[PAD_RT] = 0;
+    map->btns_mask[PAD_RJ] = 0;
 
-    face_btns_rotate_right(btns_mask);
+    face_btns_rotate_right(map);
 }
 
-void n64_8bitdo(struct bt_data *bt_data) {
-    bt_data->raw_src_mappings[PAD].mask[0] = 0x23150FFF;
-    bt_data->raw_src_mappings[PAD].desc[0] = 0x0000000F;
+static void n64_8bitdo(struct raw_src_mapping *map) {
+    map->mask[0] = 0x23150FFF;
+    map->desc[0] = 0x0000000F;
 
-    bt_data->raw_src_mappings[PAD].btns_mask[PAD_RY_UP] = BIT(8);
-    bt_data->raw_src_mappings[PAD].btns_mask[PAD_RX_RIGHT] = BIT(9);
-    bt_data->raw_src_mappings[PAD].btns_mask[PAD_RY_DOWN] = bt_data->raw_src_mappings[PAD].btns_mask[PAD_RB_LEFT];
-    bt_data->raw_src_mappings[PAD].btns_mask[PAD_RX_LEFT] = bt_data->raw_src_mappings[PAD].btns_mask[PAD_RB_UP];
-    bt_data->raw_src_mappings[PAD].btns_mask[PAD_LM] = bt_data->raw_src_mappings[PAD].btns_mask[PAD_MS];
-    bt_data->raw_src_mappings[PAD].btns_mask[PAD_RB_LEFT] = bt_data->raw_src_mappings[PAD].btns_mask[PAD_RB_RIGHT];
-    bt_data->raw_src_mappings[PAD].btns_mask[PAD_RB_RIGHT] = 0;
-    bt_data->raw_src_mappings[PAD].btns_mask[PAD_RB_UP] = 0;
-    bt_data->raw_src_mappings[PAD].btns_mask[PAD_MS] = 0;
-    bt_data->raw_src_mappings[PAD].btns_mask[PAD_RT] = 0;
+    map->btns_mask[PAD_RY_UP] = BIT(8);
+    map->btns_mask[PAD_RX_RIGHT] = BIT(9);
+    map->btns_mask[PAD_RY_DOWN] = map->btns_mask[PAD_RB_LEFT];
+    map->btns_mask[PAD_RX_LEFT] = map->btns_mask[PAD_RB_UP];
+    map->btns_mask[PAD_LM] = map->btns_mask[PAD_MS];
+    map->btns_mask[PAD_RB_LEFT] = map->btns_mask[PAD_RB_RIGHT];
+    map->btns_mask[PAD_RB_RIGHT] = 0;
+    map->btns_mask[PAD_RB_UP] = 0;
+    map->btns_mask[PAD_MS] = 0;
+    map->btns_mask[PAD_RT] = 0;
 }
 
 void mapping_quirks_apply(struct bt_data *bt_data) {
     if (atomic_test_bit(&bt_data->flags, BT_QUIRK_FACE_BTNS_INVERT)) {
-        face_btns_invert(bt_data->raw_src_mappings[PAD].btns_mask);
+        face_btns_invert(&bt_data->raw_src_mappings[PAD]);
     }
     if (atomic_test_bit(&bt_data->flags, BT_QUIRK_FACE_BTNS_ROTATE_RIGHT)) {
-        face_btns_rotate_right(bt_data->raw_src_mappings[PAD].btns_mask);
+        face_btns_rotate_right(&bt_data->raw_src_mappings[PAD]);
     }
     if (atomic_test_bit(&bt_data->flags, BT_QUIRK_TRIGGER_PRI_SEC_INVERT)) {
-        trigger_pri_sec_invert(bt_data->raw_src_mappings[PAD].btns_mask);
+        trigger_pri_sec_invert(&bt_data->raw_src_mappings[PAD]);
     }
     if (atomic_test_bit(&bt_data->flags, BT_QUIRK_SW_LEFT_JOYCON)) {
-        sw_left_joycon(bt_data->raw_src_mappings[PAD].btns_mask);
+        sw_left_joycon(&bt_data->raw_src_mappings[PAD]);
     }
     if (atomic_test_bit(&bt_data->flags, BT_QUIRK_SW_RIGHT_JOYCON)) {
-        sw_right_joycon(bt_data->raw_src_mappings[PAD].btns_mask);
+        sw_right_joycon(&bt_data->raw_src_mappings[PAD]);
     }
     if (atomic_test_bit(&bt_data->flags, BT_QUIRK_8BITDO_N64)) {
-        n64_8bitdo(bt_data);
+        n64_8bitdo(&bt_data->raw_src_mappings[PAD]);
     }
 }
