fdb68d59208c921d934dc5c7a478b37a7f831add
Jacques Gagnon <darthcloud@gmail.com>
2023-03-01 20:16:37 -0500

    [TESTS] Turn sanity into HID keyboard test

--- tests/pytest_hid_keyboard.py
+++ tests/pytest_hid_keyboard.py
@@ -1,28 +1,29 @@
-import pytest
 import json
 
 
-def test_injector_sanity(blueretro):
-    name = 'BlueRetro Test'
-    hid_desc = '05010906a101850175019508050719e0' \
-               '29e71500250181029501750881039505' \
-               '75010508190129059102950175039103' \
-               '95067508150026ff000507190029ff81' \
-               '00c0050c0901a1018502150025017501' \
-               '95160ab1010a23020aae010a8a010940' \
-               '096f0a210209b609cd09b509e209ea09' \
-               'e909300a83010a24020a06030a08030a' \
-               '01030a83010a0a030970810295017502' \
-               '8103c0'
+DEVICE_NAME = 'HID Keyboard'
+HID_DESC = '05010906a101850175019508050719e0' \
+           '29e71500250181029501750881039505' \
+           '75010508190129059102950175039103' \
+           '95067508150026ff000507190029ff81' \
+           '00c0050c0901a1018502150025017501' \
+           '95160ab1010a23020aae010a8a010940' \
+           '096f0a210209b609cd09b509e209ea09' \
+           'e909300a83010a24020a06030a08030a' \
+           '01030a83010a0a030970810295017502' \
+           '8103c0'
 
+
+def test_hid_keyboard_descriptor(blueretro):
+    blueretro.disconnect()
     blueretro.connect()
-    blueretro.send_name(name)
-    blueretro.send_hid_desc(hid_desc)
+    blueretro.send_name(DEVICE_NAME)
+    blueretro.send_hid_desc(HID_DESC)
     blueretro.disconnect()
 
     blueretro.get_logs()
 
-    blueretro.expect('# dev: 0 type: 0:0 BlueRetro Test', timeout=1)
+    blueretro.expect('# dev: 0 type: 0:0 HID Keyboard', timeout=1)
 
     report = json.loads(blueretro.expect('({.*?parsed_hid_report.*?)\n', timeout=1).group(1))
     assert report["report_id"] == 1
@@ -38,4 +39,3 @@ def test_injector_sanity(blueretro):
     assert report["report_type"] == 3
     assert report["device_type"] == -1
     assert report["device_subtype"] == 0
-
