1e6b82fad641c83e52651c4bdf99403a220d7317
Jacques Gagnon <darthcloud@gmail.com>
2023-03-09 20:34:41 -0500

    [TESTS] Add Switch SNES test

--- tests/device_data/sw.py
+++ tests/device_data/sw.py
@@ -78,6 +78,18 @@ sw_n_n64_btns_mask = [
 ]
 
 
+sw_n_snes_btns_mask = [
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    bit(sw_n.LEFT), bit(sw_n.RIGHT), bit(sw_n.DOWN), bit(sw_n.UP),
+    0, 0, 0, 0,
+    bit(sw_n.Y), bit(sw_n.A), bit(sw_n.B), bit(sw_n.X),
+    bit(sw_n.PLUS), bit(sw_n.MINUS), 0, 0,
+    bit(sw_n.L), bit(sw_n.ZL), 0, 0,
+    bit(sw_n.R), bit(sw_n.ZR), 0, 0,
+]
+
+
 sw_n_axes = {
     axis.LX: {'neutral': 0x800, 'abs_max': 0x578, 'deadzone': 0xAE},
     axis.LY: {'neutral': 0x800, 'abs_max': 0x578, 'deadzone': 0xAE},
---
+++ tests/pytest_sw_snes_controller.py
@@ -0,0 +1,58 @@
+''' Tests for the Switch SNES controller. '''
+import pytest
+from device_data.test_data_generator import btns_generic_test_data
+from bit_helper import swap24
+from device_data.sw import sw_n_snes_btns_mask
+from device_data.gc import GC
+
+
+DEVICE_NAME = 'SNES Controller'
+
+
+@pytest.mark.skip(reason="Broken since v1.8.2.")
+def test_sw_snes_controller_default_buttons_mapping(blueretro):
+    ''' Press each buttons and check if default mapping is right. '''
+    # Connect device
+    blueretro.disconnect()
+    blueretro.send_system_id(GC)
+    blueretro.connect()
+    blueretro.send_name(DEVICE_NAME)
+
+    blueretro.get_logs()
+    blueretro.expect(r'# dev: 0 type: 5:14 SNES Controller', timeout=1)
+
+    # Init adapter with a few neutral state report
+    for _ in range(2):
+        blueretro.send_hid_report(
+            'a1300180'
+            '000000'
+            '000000'
+            '000000'
+            '00000000000000000000000000'
+            '00000000000000000000000000'
+            '0000000000000000000000'
+        )
+
+    blueretro.flush_logs()
+
+    # Validate buttons default mapping
+    for sw_btns, br_btns in btns_generic_test_data(sw_n_snes_btns_mask):
+        blueretro.send_hid_report(
+            'a1300180'
+            f'{swap24(sw_btns):06x}'
+            '000000'
+            '000000'
+            '00000000000000000000000000'
+            '00000000000000000000000000'
+            '0000000000000000000000'
+        )
+
+        blueretro.get_logs()
+
+        wireless = blueretro.expect_json('wireless_input')
+        br_generic = blueretro.expect_json('generic_input')
+
+        assert wireless['btns'] >> 8 == sw_btns
+        assert br_generic['btns'][0] == br_btns
+
+    blueretro.disconnect()
