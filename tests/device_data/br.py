5656ec09401c1d19fa5833a82e1873232c6f5133
Jacques Gagnon <darthcloud@gmail.com>
2023-04-03 20:28:52 -0400

    [TESTS] Add more enum from adapter.h

--- tests/device_data/br.py
+++ tests/device_data/br.py
@@ -3,6 +3,79 @@ from enum import IntEnum, auto
 from bit_helper import bit
 
 
+class bt_type(IntEnum):
+    ''' BT device type ID. '''
+    NONE = -1
+    HID_GENERIC = 0
+    PS3 = auto()
+    WII = auto()
+    XBOX = auto()
+    PS = auto()
+    SW = auto()
+
+
+class bt_subtype(IntEnum):
+    ''' BT device subtype ID. '''
+    SUBTYPE_DEFAULT = 0
+    WII_NUNCHUCK = auto()
+    WII_CLASSIC = auto()
+    WII_CLASSIC_8BIT = auto()
+    WII_CLASSIC_PRO = auto()
+    WII_CLASSIC_PRO_8BIT = auto()
+    WIIU_PRO = auto()
+    PS5_DS = auto()
+    XBOX_XINPUT = auto()
+    XBOX_XS = auto()
+    XBOX_ADAPTIVE = auto()
+    SW_LEFT_JOYCON = auto()
+    SW_RIGHT_JOYCON = auto()
+    SW_NES = auto()
+    SW_SNES = auto()
+    SW_N64 = auto()
+    SW_MD_GEN = auto()
+    SW_POWERA = auto()
+    SW_HYPERKIN_ADMIRAL = auto()
+    GBROS = auto()
+
+
+class system(IntEnum):
+    ''' Wired system ID. '''
+    WIRED_AUTO = 0
+    PARALLEL_1P = auto()
+    PARALLEL_2P = auto()
+    NES = auto()
+    PCE = auto()
+    GENESIS = auto()
+    SNES = auto()
+    CDI = auto()
+    CD32 = auto()
+    REAL_3DO = auto()
+    JAGUAR = auto()
+    PSX = auto()
+    SATURN = auto()
+    PCFX = auto()
+    JVS = auto()
+    N64 = auto()
+    DC = auto()
+    PS2 = auto()
+    GC = auto()
+    WII_EXT = auto()
+    VBOY = auto()
+    PARALLEL_1P_OD = auto()
+    PARALLEL_2P_OD = auto()
+    SEA_BOARD = auto()
+
+
+class report_type(IntEnum):
+    ''' Report type ID. '''
+    REPORT_NONE = -1
+    KB = 0
+    MOUSE = auto()
+    PAD = auto()
+    EXTRA = auto()
+    RUMBLE = auto()
+
+
 class pad(IntEnum):
     ''' Buttons bitfield definition for controllers. '''
     LX_LEFT = 0
@@ -47,6 +120,13 @@ class axis(IntEnum):
     RY = auto()
     LM = auto()
     RM = auto()
+
+class dev_mode(IntEnum):
+    ''' Dev mode. '''
+    PAD = 0
+    PAD_ALT = auto()
+    KB = auto()
+    MOUSE = auto()
 
 
 hat_to_ld_btns = [
