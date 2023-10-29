563ba9a80131af4fcc1bd2a877fd52d8159d4c36
Jacques Gagnon <darthcloud@gmail.com>
2023-10-29 12:58:23 -0400

    [ADAPTER] Add Famicom Keyboard support

--- main/adapter/wired/npiso.c
+++ main/adapter/wired/npiso.c
@@ -16,6 +16,16 @@
 #define VTAP_PAL_PIN 16
 #define VTAP_MODE_PIN 27
 
+#define P2_D1_PIN 25
+#define P2_D2_PIN 3
+#define P2_D3_PIN 16
+#define P2_D4_PIN 27
+
+#define P2_D1_MASK (1U << P2_D1_PIN)
+#define P2_D2_MASK (1U << P2_D2_PIN)
+#define P2_D3_MASK (1U << P2_D3_PIN)
+#define P2_D4_MASK (1U << P2_D4_PIN)
+
 enum {
     NPISO_LD_RIGHT = 0,
     NPISO_LD_LEFT,
@@ -77,6 +87,16 @@ struct npiso_trackball_map {
     int32_t raw_axes[2];
 } __packed;
 
+struct npiso_kb_map {
+    uint32_t scancodes[9][2];
+} __packed;
+
+struct npiso_kb_key_to_matrix {
+    uint8_t column_idx;
+    uint8_t row_nibble_idx;
+    uint32_t row_mask;
+};
+
 static const uint32_t npiso_mask[4] = {0x333F0F00, 0x00000000, 0x00000000, BR_COMBO_MASK};
 static const uint32_t npiso_desc[4] = {0x00000000, 0x00000000, 0x00000000, 0x00000000};
 static DRAM_ATTR const uint32_t npiso_btns_mask[32] = {
@@ -129,8 +149,51 @@ static const uint32_t npiso_trackball_btns_mask[32] = {
     BIT(NPISO_B), 0, 0, 0,
 };
 
+static const uint32_t npiso_kb_mask[4] = {0xE6FF0F0F, 0xFFFFFFFF, 0xFFFFFFFF, 0x0007FFFF | BR_COMBO_MASK};
+static const uint32_t npiso_kb_desc[4] = {0x00000000, 0x00000000, 0x00000000, 0x00000000};
+static const struct npiso_kb_key_to_matrix npiso_kb_key_to_matrix[KBM_MAX] = {
+ /* KB_A, KB_D, KB_S, KB_W, MOUSE_X_LEFT, MOUSE_X_RIGHT, MOUSE_Y_DOWN MOUSE_Y_UP */
+    {6, 0, P2_D4_MASK}, {5, 0, P2_D4_MASK}, {6, 0, P2_D3_MASK}, {6, 0, P2_D2_MASK}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
+ /* KB_LEFT, KB_RIGHT, KB_DOWN, KB_UP, MOUSE_WX_LEFT, MOUSE_WX_RIGHT, MOUSE_WY_DOWN, MOUSE_WY_UP */
+    {8, 0, P2_D4_MASK}, {8, 0, P2_D3_MASK}, {8, 1, P2_D1_MASK}, {8, 0, P2_D2_MASK}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
+ /* KB_Q, KB_R, KB_E, KB_F, KB_ESC, KB_ENTER, KB_LWIN, KB_HASH */
+    {7, 0, P2_D3_MASK}, {5, 0, P2_D3_MASK}, {6, 1, P2_D3_MASK}, {5, 1, P2_D1_MASK}, {7, 0, P2_D2_MASK}, {0, 0, P2_D2_MASK}, {0, 0, 0}, {0, 1, P2_D3_MASK},
+ /* MOUSE_RIGHT, KB_Z, KB_LCTRL, MOUSE_MIDDLE, MOUSE_LEFT, KB_X, KB_LSHIFT, KB_SPACE */
+    {0, 0, 0}, {6, 1, P2_D2_MASK}, {7, 0, P2_D4_MASK}, {0, 0, 0}, {0, 0, 0}, {6, 1, P2_D1_MASK}, {7, 1, P2_D1_MASK}, {8, 1, P2_D2_MASK},
+
+ /* KB_B, KB_C, KB_G, KB_H, KB_I, KB_J, KB_K, KB_L */
+    {4, 1, P2_D1_MASK}, {5, 1, P2_D2_MASK}, {4, 0, P2_D3_MASK}, {4, 0, P2_D4_MASK}, {3, 0, P2_D2_MASK}, {3, 0, P2_D4_MASK}, {2, 0, P2_D4_MASK}, {2, 0, P2_D3_MASK},
+ /* KB_M, KB_N, KB_O, KB_P, KB_T, KB_U, KB_V, KB_Y */
+    {3, 1, P2_D1_MASK}, {3, 1, P2_D2_MASK}, {2, 0, P2_D2_MASK}, {2, 1, P2_D3_MASK}, {5, 0, P2_D2_MASK}, {3, 0, P2_D3_MASK}, {4, 1, P2_D2_MASK}, {4, 0, P2_D2_MASK},
+ /* KB_1, KB_2, KB_3, KB_4, KB_5, KB_6, KB_7, KB_8 */
+    {7, 1, P2_D3_MASK}, {7, 1, P2_D4_MASK}, {6, 1, P2_D4_MASK}, {5, 1, P2_D4_MASK}, {5, 1, P2_D3_MASK}, {4, 1, P2_D4_MASK}, {4, 1, P2_D3_MASK}, {3, 1, P2_D4_MASK},
+ /* KB_9, KB_0, KB_BACKSPACE, KB_TAB, KB_MINUS, KB_EQUAL, KB_LEFTBRACE, KB_RIGHTBRACE */
+    {3, 1, P2_D3_MASK}, {2, 1, P2_D4_MASK}, {0, 1, P2_D4_MASK}, {0, 0, 0}, {1, 1, P2_D4_MASK}, {1, 1, P2_D3_MASK}, {0, 0, P2_D3_MASK}, {0, 0, P2_D4_MASK},
+
+ /* KB_BACKSLASH, KB_SEMICOLON, KB_APOSTROPHE, KB_GRAVE, KB_COMMA, KB_DOT, KB_SLASH, KB_CAPSLOCK */
+    {1, 1, P2_D1_MASK}, {1, 0, P2_D3_MASK}, {1, 0, P2_D4_MASK}, {0, 1, P2_D3_MASK}, {2, 1, P2_D2_MASK}, {2, 1, P2_D1_MASK}, {1, 1, P2_D2_MASK}, {0, 1, P2_D1_MASK},
+ /* KB_F1, KB_F2, KB_F3, KB_F4, KB_F5, KB_F6, KB_F7, KB_F8 */
+    {7, 0, P2_D1_MASK}, {6, 0, P2_D1_MASK}, {5, 0, P2_D1_MASK}, {4, 0, P2_D1_MASK}, {3, 0, P2_D1_MASK}, {2, 0, P2_D1_MASK}, {1, 0, P2_D1_MASK}, {0, 0, P2_D1_MASK},
+ /* KB_F9, KB_F10, KB_F11, KB_F12, KB_PSCREEN, KB_SCROLL, KB_PAUSE, KB_INSERT */
+    {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {8, 1, P2_D4_MASK},
+ /* KB_HOME, KB_PAGEUP, KB_DEL, KB_END, KB_PAGEDOWN, KB_NUMLOCK, KB_KP_DIV, KB_KP_MULTI */
+    {8, 0, P2_D1_MASK}, {0, 0, 0}, {8, 1, P2_D3_MASK}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
+
+ /* KB_KP_MINUS, KB_KP_PLUS, KB_KP_ENTER, KB_KP_1, KB_KP_2, KB_KP_3, KB_KP_4, KB_KP_5 */
+    {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
+ /* KB_KP_6, KB_KP_7, KB_KP_8, KB_KP_9, KB_KP_0, KB_KP_DOT, KB_LALT, KB_RCTRL */
+    {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {7, 1, P2_D3_MASK}, {0, 0, 0}, {0, 0, 0},
+ /* KB_RSHIFT, KB_RALT, KB_RWIN */
+    {0, 1, P2_D2_MASK}, {1, 0, P2_D2_MASK}, {0, 0, 0},
+};
+
 void IRAM_ATTR npiso_init_buffer(int32_t dev_mode, struct wired_data *wired_data) {
     switch (dev_mode) {
+        case DEV_KB:
+        {
+            memset(wired_data->output, 0x00, sizeof(struct npiso_kb_map));
+            break;
+        }
         case DEV_MOUSE:
         {
             if (wired_adapter.system_id == SNES) {
@@ -178,6 +241,10 @@ void npiso_meta_init(struct wired_ctrl *ctrl_data) {
     for (uint32_t i = 0; i < WIRED_MAX_DEV; i++) {
         for (uint32_t j = 0; j < ADAPTER_MAX_AXES; j++) {
             switch (config.out_cfg[i].dev_mode) {
+                case DEV_KB:
+                    ctrl_data[i].mask = npiso_kb_mask;
+                    ctrl_data[i].desc = npiso_kb_desc;
+                    break;
                 case DEV_MOUSE:
                     if (wired_adapter.system_id == SNES) {
                         ctrl_data[i].mask = npiso_mouse_mask;
@@ -332,8 +399,28 @@ static void npiso_trackball_from_generic(struct wired_ctrl *ctrl_data, struct wi
     memcpy(wired_data->output, (void *)&map_tmp, sizeof(map_tmp) - 8);
 }
 
+static void npiso_kb_from_generic(struct wired_ctrl *ctrl_data, struct wired_data *wired_data) {
+    struct npiso_kb_map map_tmp = {0};
+
+    for (uint32_t i = 0; i < KBM_MAX; i++) {
+        if (ctrl_data->map_mask[i / 32] & BIT(i & 0x1F)) {
+            if (ctrl_data->btns[i / 32].value & BIT(i & 0x1F)) {
+                uint8_t column = npiso_kb_key_to_matrix[i].column_idx;
+                uint8_t row = npiso_kb_key_to_matrix[i].row_nibble_idx;
+
+                map_tmp.scancodes[column][row] |= npiso_kb_key_to_matrix[i].row_mask;
+            }
+        }
+    }
+
+    memcpy(wired_data->output, (void *)&map_tmp, sizeof(map_tmp));
+}
+
 void npiso_from_generic(int32_t dev_mode, struct wired_ctrl *ctrl_data, struct wired_data *wired_data) {
     switch (dev_mode) {
+        case DEV_KB:
+            npiso_kb_from_generic(ctrl_data, wired_data);
+            break;
         case DEV_MOUSE:
             if (wired_adapter.system_id == SNES) {
                 npiso_mouse_from_generic(ctrl_data, wired_data);
