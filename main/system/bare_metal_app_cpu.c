c6bc09492fe6bd87ff77ce4ede73e4f3468df91a
Jacques Gagnon <darthcloud@gmail.com>
2024-02-19 21:45:59 -0500

    [SYS] Update headers for IDF 5.3.0

--- main/system/bare_metal_app_cpu.c
+++ main/system/bare_metal_app_cpu.c
@@ -35,7 +35,7 @@
 #include <esp32/rom/cache.h>
 #include <xt_instr_macros.h>
 #include <xtensa/config/specreg.h>
-#include <xtensa/xtensa_api.h>
+#include <xtensa_api.h>
 #include <xtensa/config/core.h>
 
 typedef void (*wired_init_t)(void);
